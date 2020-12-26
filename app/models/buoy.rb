8539c9c0d3e3f11275f3f74b2b3e6d66349a01cf
tobias ferreira <soutobias@yahoo.com.br>
2020-12-26 15:53:02 -0300

    external db

--- app/models/buoy.rb
+++ app/models/buoy.rb
@@ -1,3 +1,4 @@
-class Buoy < ApplicationRecord
+class Buoy < ActiveRecord::Base
+  establish_connection :external
   has_many :data_buoys
 end
--- app/models/data_buoy.rb
+++ app/models/data_buoy.rb
@@ -1,3 +1,4 @@
-class DataBuoy < ApplicationRecord
+class DataBuoy < ActiveRecord::Base
+  establish_connection :external
   belongs_to :buoy
 end
