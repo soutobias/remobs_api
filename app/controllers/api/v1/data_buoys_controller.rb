95227181d86c8c49548da9ce59b421a3dda884dd
tobias ferreira <soutobias@yahoo.com.br>
2021-12-08 13:29:36 -0300

    no start end date

--- app/controllers/api/v1/data_buoys_controller.rb
+++ app/controllers/api/v1/data_buoys_controller.rb
@@ -15,6 +15,7 @@ class Api::V1::DataBuoysController < Api::V1::BaseController
             end
             @query += "date_time >= '#{start_date.strftime("%Y-%m-%d %H:%M:%S")}' AND "
           else
+            start_date = Time.now.utc - (3600*24*4)
             @query += "date_time >= '#{(Time.now.utc - (3600*24*5)).strftime("%Y-%m-%d %H:%M:%S")}' AND "
           end
           if params[:end_date].present?
@@ -26,13 +27,14 @@ class Api::V1::DataBuoysController < Api::V1::BaseController
             end
             @query += "date_time <= '#{end_date.strftime("%Y-%m-%d %H:%M:%S")}' AND "
           else
-            @query += "date_time <= '#{(start_date + (3600*24*1)).strftime("%Y-%m-%d %H:%M:%S")}' AND "
+            @query += "date_time <= '#{(Time.now.utc + (3600*24*1)).strftime("%Y-%m-%d %H:%M:%S")}' AND "
           end
+
           @query += "buoy_id = #{params[:buoy]}"
-          print(@query)
           if @query.downcase.include? 'drop'
             @data_buoys = []
           else
+            puts(@query)
             @data_buoys = policy_scope(DataBuoy).where(@query).order(date_time: :desc)
           end
         end
