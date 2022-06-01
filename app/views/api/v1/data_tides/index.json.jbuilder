4befbcbf0b218cd93ee427df3ca06912cc30d0f2
tobias ferreira <soutobias@yahoo.com.br>
2022-05-31 22:50:47 -0300

    tide api included

--- app/views/api/v1/data_tides/index.json.jbuilder
+++ app/views/api/v1/data_tides/index.json.jbuilder
@@ -1,8 +1,8 @@
 json.array! @data_tides do |data_tide|
   json.id data_tide.id
   json.buoy_id data_tide.buoy_id
-  json.date_time data_buoy.date_time
-  json.elev1 data_buoy.swvht1
-  json.elev2 data_buoy.tp1
+  json.date_time data_tide.date_time
+  json.elev1 data_tide.swvht1
+  json.elev2 data_tide.tp1
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :buoys, only: [ :index, :show ]
+      resources :data_tides, only: [ :index, :show ]
       resources :data_buoys, only: [ :index, :show ]
       resources :spotters, only: [ :index, :show ]
       resources :waves, only: [ :index, :show ]
