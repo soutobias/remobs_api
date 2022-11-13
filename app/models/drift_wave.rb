e601fe0428c784f3421125649d8f229f66537400
tobias ferreira <soutobias@yahoo.com.br>
2022-11-13 13:53:41 -0300

    correct drift wave name

--- app/models/drift_wave.rb
+++ app/models/drift_wave.rb
@@ -1,4 +1,4 @@
-class DriftValue < ActiveRecord::Base
+class DriftWave < ActiveRecord::Base
     establish_connection :external_new
     self.table_name = "drift.spotter_waves"
     belongs_to :drift_buoy
